97e6b322020018f793c573197ef3a74bf8f045bd
Ivan Pimenov <strauc@mail.ru>
2024-10-08 00:44:24 +0400

    add eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,6 @@ import { Command } from 'commander';
 
 const program = new Command();
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+program.name('gendiff').description('Compares two configuration files and shows a difference.').version('1.0.0');
 
 program.parse();
